61f14a3ab8af7efa34c3ad7a44f98a43a060e192
sevimgokturk <svmldg@gmail.com>
2022-03-31 15:17:31 +0200

    new changing

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("git remote remove origin");
         System.out.println(" git add .");
         System.out.println("git commit -m '.....'");
         System.out.println("git push");
+        Scanner scanner =new Scanner(System.in);
 
 
 
