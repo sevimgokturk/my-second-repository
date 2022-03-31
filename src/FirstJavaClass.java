89306046cb3c7301660e46bda13783490a438e47
sevimgokturk <svmldg@gmail.com>
2022-03-31 13:05:23 +0200

    some changing

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,6 +1,9 @@
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("git remote remove origin");
+        System.out.println(" git add .");
+        System.out.println("git commit -m '.....'");
+        System.out.println("git push");
 
     }
 }
