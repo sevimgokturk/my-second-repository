3a5723161aa5119a02d2e8f8aa23e3c29298b5e3
sevimgokturk <svmldg@gmail.com>
2022-04-01 03:33:52 +0200

    first commit

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("git remote remove origin");
-        System.out.println(" git add .");
-        System.out.println("git commit -m '.....'");
-        System.out.println("git push");
+
 
 
 
