a8d78c55eeb248db60ef76acd3707f5c7a9d3922
sevimgokturk <svmldg@gmail.com>
2022-04-01 03:37:02 +0200

    second commid

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -3,12 +3,6 @@ import java.util.Scanner;
 public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("git remote remove origin");
-
-
-
-
-
-
-
+        System.out.println("some changing");
     }
 }
